d780cc590aa929050bd50527ff6cb608fd6d4b48
saperez17 <saperez@unicauca.edu.co>
2021-05-21 17:00:29 -0500

    added top categories and improved main content area

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,19 @@
-$(document).ready(()=>{
-    $(".edit-btn")[0].onclick = ()=>{
-        var p = $(".blog-post-body:first");
-        p.replaceWith($("<textarea/>", {
-            "class": "edit",
-            "text": p.text().replace("\n", "").replace(/\s{2,}/g, " ").trim(),
-            "css": { "width": p.css('width'), "height":p.css('height') }
-        }));
-    }
+// $(document).ready(()=>{
+//     $(".edit-btn")[0].onclick = ()=>{
+//         var p = $(".blog-post-body:first");
+//         p.replaceWith($("<textarea/>", {
+//             "class": "edit",
+//             "text": p.text().replace("\n", "").replace(/\s{2,}/g, " ").trim(),
+//             "css": { "width": p.css('width'), "height":p.css('height') }
+//         }));
+//     }
     
-})
+// })
+
+var articleCards = $(".article-card");
+
+articleCards.each(function(idx, val){
+   $(val).click(()=>{
+       console.log($(val)[0].dataset)
+   })
+});
